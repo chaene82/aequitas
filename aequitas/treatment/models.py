97515fb50dc5ba77ad5f8659e03455a2353289c2
Christoph Häne <christoph.haene@gmail.com>
2022-11-25 14:20:39 +0100

    install scheduler

---
+++ aequitas/treatment/models.py
@@ -0,0 +1,18 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Institute(models.Model):
+    title = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    def __str__(self):
+        return self.title
+
+class Patient(models.Model):
+    first_name = models.CharField
+    last_name = models.CharField
+    
+
+
+
