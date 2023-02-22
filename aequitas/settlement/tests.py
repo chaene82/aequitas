de488a245cd7c1c2f0daa20c1031c7e0379ef964
Christoph Haene <christoph.haene@ckw.ch>
2023-02-22 21:40:54 +0100

    add invoice hancling (without options)

--- aequitas/settlement/tests.py
+++ aequitas/settlement/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+import os, datetime
 from settlement.models import Patient, LegalGuardiant
+from settlement.views import Invoice
 
 # Create your tests here.
 
@@ -7,6 +9,8 @@ class TestSettlement(TestCase):
     """
     This are general test cases for the Aequtias model Treatments.
     """
+    #fixtures = ['test/testdata_settlement.json']
+
 
     def test_setup(self):
 
@@ -16,20 +20,43 @@ class TestSettlement(TestCase):
         """Test adding a customer
         create a patient and count the number of the patient.
         """
-        a = LegalGuardiant(display_name = "Test Guardiant", address = "Test Address")
+        a = LegalGuardiant(display_name = "Test Guardiant")
         a.save()
 
     def test_patient_add(self):
         """Test adding a customer
         create a patient and count the number of the patient.
         """
-        a = Patient(display_name = "Test Patient", address = "Test Address")
+        a = Patient(display_name = "Test Patient")
         a.save()
 
         nr_patient = Patient.objects.count()
         print("Number of Records = ", nr_patient)
         self.assertEqual(nr_patient, 1)
-
+    
+    
+class TestInvoice(TestCase):     
+    def test_create_invoice(self):
+        dt_now = datetime.datetime.now()
+        seq = int(dt_now.strftime("%Y%m%d%H%M%S"))
+        
+        template = 'Rechnung+HE+318.632.2_D+2023_r.pdf'
+        document = 'test_create_invoice_' + str(seq) + '.pdf'
+        
+        i = Invoice(template, document)
+        i.forms_path = 'forms/'
+        i.document_path = 'test/documents/'
+        fields = i.get_fields()
+        
+        fields['11AHVNr'] = '1234'
+        result = i.create(fields)
+              
+        self.assertTrue(os.path.exists('test/documents/' + document))
+        
+        
+        
+        
+        
 
 
 
--- aequitas/settlement/views.py
+++ aequitas/settlement/views.py
@@ -6,6 +6,48 @@ from .serializers import PatientSerializer, CostApprovalSerializer, AddressSeria
         SettlementSerializer
 from rest_framework import viewsets
 
+from fillpdf import fillpdfs
+
+
+#fileds = fillpdfs.get_form_fields("../forms/Rechnung+HE+318.632.2_D+2023_r.pdf")
+#fileds['11AHVNr'] = '1234'
+
+#fillpdfs.write_fillable_pdf("../forms/Rechnung+HE+318.632.2_D+2023_r.pdf", '../documents/new.pdf', fileds)
+
+#p = Patient(display_name = "Test Patient", address = "Test Address")
+
+class Invoice:
+    template = ''
+    document = ''
+    forms_path = '../forms/'
+    document_path = '../documents/'
+    filed = ''
+
+    def __init__(self, template, document):
+        self.template = template
+        self.document = document
+        
+    def create(self, fields):
+        template_file_path = self.forms_path + self.template
+        document_file_path = self.document_path + self.document
+
+        fillpdfs.write_fillable_pdf(template_file_path , document_file_path, fields)
+
+        return document_file_path
+    
+    def get_fields(self):
+        template_file_path = self.forms_path + self.template
+        fileds = fillpdfs.get_form_fields(template_file_path)
+        return fileds
+        
+
+        
+    def hle(self, Patient, LegalGuardiant, CostApproval, PaymentMethode):
+        return None
+        
+    
+
+
 class CostApprovalTypeViewSet(viewsets.ModelViewSet):
     queryset = CostApprovalType.objects.all()
     serializer_class = CostApprovalTypeSerializer
