d3d0df4eb9a9a9272902ce78b0ad12c0709db55f
chaene82 <christoph.haene@gmail.com>
2024-02-09 13:09:47 +0100

    move database

--- aequitas/settings.py
+++ aequitas/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DB_DIR = BASE_DIR / 'aequitas/database'
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pz8&^^(1l!694j0d%w3bl##b*pw976^ybk50^7*y!n#o*y!r69'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,7 +86,7 @@ WSGI_APPLICATION = 'aequitas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_DIR / 'db.sqlite3',
     }
 }
 
