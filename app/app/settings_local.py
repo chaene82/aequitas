3f37f19100619ac0f88347843d016833fa53e16d
Christoph Haene <christoph.haene@ckw.ch>
2024-09-02 20:34:54 +0200

    add local settings for dev

--- app/app/settings_local.py
+++ app/app/settings_local.py
@@ -1,20 +1,13 @@
 import os
 from pathlib import Path
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1234567"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
