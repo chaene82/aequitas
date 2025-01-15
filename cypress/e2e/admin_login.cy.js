5b9cf50f99ffca97db056cdf9b6a5a6cbdb91e80
Christoph Haene <christoph.haene@ckw.ch>
2025-01-15 21:21:28 +0100

    add a test

--- cypress/e2e/admin_login.cy.js
+++ cypress/e2e/admin_login.cy.js
@@ -2,4 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://test.aquitas.dynv6.net:28080/admin')
   })
+  
+})
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
 })
